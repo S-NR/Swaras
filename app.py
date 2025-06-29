583a28102d2d4e0c6530743ef9553c04b613ebab
SNR <84119114+S-NR@users.noreply.github.com>
2025-06-29 13:08:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, send_file
 import librosa
 import numpy as np
 import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
